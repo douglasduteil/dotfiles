19915d3360b6410f108b55c90d71bbde171ac8d9
Douglas Duteil <douglasduteil@gmail.com>
2016-07-01 22:22:48 +0200

    style: convert tabs to spaces

--- install.sh
+++ install.sh
@@ -5,13 +5,13 @@
 export DOTFILES_DIR EXTRA_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# Update dotfiles itself first
+# Updat e dotfiles itself first
 
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # Bunch of symlinks
 
-ln -sfv "$DOTFILES_DIR/zsh/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/zsh/.zsh" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 
