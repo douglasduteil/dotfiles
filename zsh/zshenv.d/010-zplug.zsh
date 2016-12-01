a02ddb56ac80a9606652a8e25949b220dfa56f43
Douglas Duteil <douglasduteil@gmail.com>
2016-12-01 23:00:51 +0100

    feat(zplug): enforce caching

--- zsh/zshenv.d/010-zplug.zsh
+++ zsh/zshenv.d/010-zplug.zsh
@@ -3,6 +3,8 @@
 # =================================================================
 
 export ZPLUG_HOME="$XDG_PROGRAMS_DIR/zplug"
+export ZPLUG_USE_CACHE=true
+export ZPLUG_CACHE_FILE="$XDG_CACHE_HOME/zplug"
 
 # =================================================================
 #
