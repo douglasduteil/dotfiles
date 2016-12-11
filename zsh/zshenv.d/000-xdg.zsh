afe22ad477cfdc9bc16f9aa3958f57d20207131d
Douglas Duteil <douglasduteil@gmail.com>
2016-12-11 14:59:56 +0100

    fix(env): typo in XDG config dir name

--- zsh/zshenv.d/000-xdg.zsh
+++ zsh/zshenv.d/000-xdg.zsh
@@ -7,7 +7,7 @@ export DOTFILES_DIR=~/.dotfiles
 
 export XDG_BIN_DIR="$HOME/bin"
 export XDG_CACHE_HOME="$HOME/.cache"
-export XDG_CONFIG_DIR="$HOME/config"
+export XDG_CONFIG_DIR="$HOME/.config"
 export XDG_DOTFILES_DIR="$HOME/.dotfiles"
 export XDG_PROGRAMS_DIR="$HOME/programs"
 
