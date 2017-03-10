6bedc22a7dc4aae6e31ac154005db10a66fdda5e
Douglas Duteil <douglasduteil@gmail.com>
2017-03-10 02:00:27 +0100

    chore(xdg): add XDG_LOCAL_DIR for local programs data

--- zsh/zshenv.d/000-xdg.zsh
+++ zsh/zshenv.d/000-xdg.zsh
@@ -8,7 +8,8 @@ export DOTFILES_DIR=~/.dotfiles
 export XDG_BIN_DIR="$HOME/bin"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CONFIG_DIR="$HOME/.config"
-export XDG_DOTFILES_DIR="$HOME/.dotfiles"
+export XDG_CONFIG_DIR="$HOME/.config"
+export XDG_LOCAL_DIR="$HOME/.local"
 export XDG_PROGRAMS_DIR="$HOME/programs"
 
 # =================================================================
