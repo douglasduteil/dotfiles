7a3d3baeabefc90ac1e3de08e34dd7fcc89b4944
Douglas Duteil <douglasduteil@gmail.com>
2020-04-08 23:23:49 +0200

    update xdg

--- zsh/zshenv.d/000-xdg.zsh
+++ zsh/zshenv.d/000-xdg.zsh
@@ -3,13 +3,14 @@
 # =================================================================
 
 # User specific data files
-export DOTFILES_DIR=~/.dotfiles
+export DOTFILES_DIR="$HOME/.dotfiles"
 
 export XDG_BIN_DIR="$HOME/bin"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_CONFIG_DIR="$HOME/.config"
-export XDG_CONFIG_DIR="$HOME/.config"
 export XDG_LOCAL_DIR="$HOME/.local"
+export XDG_DATA_HOME="$HOME/.local/share"
+export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_PROGRAMS_DIR="$HOME/programs"
 export XDG_TEMPLATES_DIR="$HOME/Templates"
 
