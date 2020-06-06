2a3d88bf0435065de8d8f0f80a86b92ab2fd3a01
Douglas Duteil <douglasduteil@gmail.com>
2020-06-06 12:10:04 +0200

    fix(clear): rmm bug

--- zsh/zshrc.d/9000-clean-script.zsh
+++ zsh/zshrc.d/9000-clean-script.zsh
@@ -5,7 +5,7 @@
 clear-cache() {
 
   rmm () {
-    rm -rv $1
+    rm -rfv "$@"
   }
 
   echo "* Remove Yay cache"
@@ -27,45 +27,45 @@ clear-cache() {
   npm cache clean --force
 
   echo "* Removing Opera Developer cache"
-  rmm ~/.config/opera-developer/File\ System/*
-  rmm ~/.config/opera-developer/Service\ Worker/CacheStorage/*
-  rmm ~/.config/opera-developer/Service\ Worker/ScriptCache/*
-  rmm ~/.config/opera-developer/GPUCache/*
+  rmm "~/.config/opera-developer/File System/*"
+  rmm "~/.config/opera-developer/Service Worker/CacheStorage/*"
+  rmm "~/.config/opera-developer/Service Worker/ScriptCache/*"
+  rmm "~/.config/opera-developer/GPUCache/*"
 
   echo "* Removing Chromium cache"
-  rmm ~/.config/chromium/*/Application\ Cache/*
-  rmm ~/.config/chromium/*/Cache/*
-  rmm ~/.config/chromium/*/Code\ Cache/*
-  rmm ~/.config/chromium/*/File\ System/*
-  rmm ~/.config/chromium/*/GPUCache/*
-  rmm ~/.config/chromium/*/Media\ Cache/*
-  rmm ~/.config/chromium/*/Service\ Worker/CacheStorage/*
-  rmm ~/.config/chromium/*/Service\ Worker/ScriptCache/*
+  rmm "~/.config/chromium/*/Application Cache/*"
+  rmm "~/.config/chromium/*/Cache/*"
+  rmm "~/.config/chromium/*/Code Cache/*"
+  rmm "~/.config/chromium/*/File System/*"
+  rmm "~/.config/chromium/*/GPUCache/*"
+  rmm "~/.config/chromium/*/Media Cache/*"
+  rmm "~/.config/chromium/*/Service Worker/CacheStorage/*"
+  rmm "~/.config/chromium/*/Service Worker/ScriptCache/*"
 
   echo "* Removing all github node_modules"
-  rmm ~/zzz/github/*/*/node_modules
+  rmm "~/zzz/github/*/*/node_modules"
 
   echo "* Removing ~/tmp/* node_modules"
-  rmm ~/tmp/*/node_modules
+  rmm "~/tmp/*/node_modules"
 
   echo "* Removing ~/.local/share/Trash node_modules"
-  rmm ~/.local/share/Trash/files/*
-  rmm ~/.local/share/Trash/info/*
+  rmm "~/.local/share/Trash/files/*"
+  rmm "~/.local/share/Trash/info/*"
 
   echo "* Removing ~/.Trash node_modules"
-  rmm ~/.Trash
+  rmm "~/.Trash"
 
   echo "* Removing ~/.cache/bazel"
-  rmm ~/.cache/bazel
+  rmm "~/.cache/bazel"
 
   echo "* Removing ~/.cache/Cypress"
-  rmm ~/.cache/Cypress
+  rmm "~/.cache/Cypress"
 
   echo "* Removing ~/.cache/typescript"
-  rmm ~/.cache/typescript
+  rmm "~/.cache/typescript"
 
   echo "* Removing google-chrome-beta cache"
-  rmm ~/.cache/google-chrome-beta/(Default|Profile*)/File\ System/*
+  rmm "~/.cache/google-chrome-beta/(Default|Profile*)/File System/*"
 
   echo "=== All Cache/Tmp Deleted ==="
 }
