210f6b0358c7f1f5fae7d6be73dd06569bd8a080
X <x@MacBook-Pro-de-X.local>
2021-04-16 18:10:33 +0200

    chore(nix): nix flake update 04/16/21 18:10:33

---
+++ nixpkgs/system.nix
@@ -0,0 +1,19 @@
+
+{ pkgs, ... }:
+
+{
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = builtins.readFile ../config/nix/nix.conf;
+  services.nix-daemon.enable = true;
+  environment.systemPackages = with pkgs; [
+    nixFlakes
+    home-manager
+    htop
+    vim
+  ];
+  system.stateVersion = 4;
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+  };
+}
