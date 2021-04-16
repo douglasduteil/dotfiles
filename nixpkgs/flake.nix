efaf614ec43ddc66cb2e6053af5d27c70752fa6a
X <x@MacBook-Pro-de-X.local>
2021-04-16 17:30:27 +0200

    chore(nix): nix flake update 04/16/21 17:30:27

--- nixpkgs/flake.nix
+++ nixpkgs/flake.nix
@@ -2,38 +2,69 @@
   description = "dougalsduteil home-manager";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+
   };
 
-  inputs.LS_COLORS = {
-    url = "github:trapd00r/LS_COLORS";
-    flake = false;
-  };
 
-  outputs = { self, home-manager, ... }@inputs:
+  outputs = inputs@{ self, nixpkgs, home-manager, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          scripts = import ./lib/scripts.nix { inherit pkgs; };
+        in
+        {
+          devShell = pkgs.mkShell {
+            name = "teste";
+            nativeBuildInputs = [
+              pkgs.git
+              scripts.update-flake
+              scripts.home-switch
+              scripts.update-system
+              scripts.flake-mgr
+            ];
+          };
+        }
+      ) //
     {
-      homeConfigurations = {
-        macbook-pro = inputs.home-manager.lib.homeManagerConfiguration {
-          configuration = { pkgs, ... }:
-            {
-              xdg.configFile."nix/nix.conf".source = ./configs/nix/nix.conf;
-              nixpkgs.config = import ./configs/nix/config.nix;
-              imports = [
-                ./home.nix
-                ./home-manager-modules/home-manager
-                ./home-manager-modules/nixos-bash
-                ./home-manager-modules/vim
-              ];
+      nixosConfigurations = {
+        watchmen = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./host/configuration.nix
+          ];
+          specialArgs = { inherit inputs; };
+        };
+      };
 
+      homeManagerConfigurations = {
+        thiago = home-manager.lib.homeManagerConfiguration {
+          configuration = { pkgs, lib, ... }: {
+            nixpkgs = {
+              config = {
+                allowUnfree = true;
+              };
             };
-          system = "x86_64-darwin";
-          homeDirectory = "/Users/x";
+            imports = [
+              ./home/home.nix
+              ./overlays
+            ];
+          };
+          system = "x86_64-linux";
+          homeDirectory = "/home/x/";
           username = "x";
         };
       };
-      macbook-pro = self.homeConfigurations.macbook-pro.activationPackage;
     };
-
 }
